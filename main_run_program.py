bb41ad5d4994607b61be2cf48809a0d301adb635
ElOsitoLoco <florian.openclassrooms.com>
2020-11-28 13:56:50 +0000

    renaming of project

--- main_run_program.py
+++ main_run_program.py
@@ -8,34 +8,41 @@ import scrap_category_pagination as num_pages
 import scrap_category as scrap_cat
 import scrap_soup as soup
 
-#this module is a work in progress
+
+
+
+def get_and_write_books(csv_writer, page_url):
+    books_urls = scrap_cat.get_href_books(page_url)
+    for book_url in books_urls:
+        book_info = scrap_book.get_book_info(book_url)
+        csv_writer.writeDict(book_info)
+
+
 def run():
-    """Calls all other functions to return all categories and book information from selected website"""
-    url = "http://books.toscrape.com/"
-
-    categories = all_c.get_category_details(url)
-
-    with open('result/output_file', 'w', newline ='') as category_csv:
-	    for category_name, category_url in categories.items():
-	    	csv_doc = csv.writer(category_csv)
-	    	category_pages = num_pages.get_pages_number(category_url)
-
-	    	for i in range(category_pages):
-	    		category_page_url = f"http://books.toscrape.com/category/{category_name}/page-{i + 1}.html"
-	    		books_urls = scrap_cat.get_href_books(category_page_url)
-
-	    		for book_url in books_urls:
-	    			book_info = scrap_book.get_book_info(book_url)
-	    			csv.writeDict(book_info)
+    website_url = "http://books.toscrape.com/"
 
 
-    #book_info = scrap_book.get_book_info(url)
-    #keys = book_info.keys()
-    #with open('result/output_file.csv', 'w', newline='') as csvfile:
-        #writer = csv.DictWriter(csvfile, keys, dialect='excel')
-        #writer.writeheader()
-        #writer.writerows([book_info])
+    categories = all_c.get_category_details(website_url)
+    for category_name, category_url in categories.items():
+        with open(f'result/{category_name}.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            get_and_write_books(csv_writer, category_url)
+
+            category_pages = num_pages.get_pages_number(category_url)
+            if category_pages > 1:
+                for i in range(category_pages):
+                    page_url = category_url[:-10] + f"page-{i + 2}.html"
+                    get_and_write_books(page_url)
+
+
 
 
 if __name__ == "__main__":
-    run()
+   run()
+
+#book_info = scrap_book.get_book_info(url)
+#keys = book_info.keys()
+#with open('result/output_file.csv', 'w', newline='') as csvfile:
+#    #writer = csv.DictWriter(csvfile, keys, dialect='excel')
+#    #writer.writeheader()
+#    #writer.writerows([book_info])
--- scrapping_program_book.py
+++ scrapping_program_book.py
@@ -3,19 +3,12 @@ import requests
 import urllib3
 import csv
 import scrap_category as scrap_cat
-
-
-def get_content(url):
-	"""gets html information from werbsite and returns parsed object"""
-	response = requests.get(url)
-	if response.status_code != 200:
-		raise Exception("couldn't pull information!/ URL not valid")
-	return BeautifulSoup(response.content, features='html.parser')
+import scrap_soup as soup
 
 
 def get_book_rating(url):
 	"""gets and returns the rating from selected book"""
-	book_info_rating = str((get_content(url)))
+	book_info_rating = str((soup.get_soup(url)))
 	if "star-rating Four" in book_info_rating:
 		return 4
 	elif "star-rating Three" in book_info_rating:
@@ -32,7 +25,7 @@ def get_book_info(url):
     convert pulled elements to strings of text
     associate each value to a key in a dictionnary and append it in the empty list
     """
-    parsed_content = get_content(url)
+    parsed_content = soup.get_soup(url)
     book_info_rating = get_book_rating(url)
 
     book_info_title = parsed_content.select('h1')[0].text.strip()
@@ -55,6 +48,4 @@ def get_book_info(url):
         "Price Incuding Tax" : book_price_incl_tax,
         "Availability" : book_available,
         "Review Rating" : book_info_rating
-    }
-
-
+        }
